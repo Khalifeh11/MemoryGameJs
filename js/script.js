7b56d3d110754ca35051816b1f630d8a700b6b14
Karim <abdelkarim.kh95@gmail.com>
2022-01-27 20:52:52 +0200

    comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// on key down game starts 
-// level 1: one sound activated 
-// if player presses that box, move to level 2 else game over
 var sounds = ["sounds/green.mp3", "sounds/red.mp3", "sounds/yellow.mp3", "sounds/blue.mp3"];
 
 var divs = document.querySelectorAll(".btn");
@@ -21,121 +18,128 @@ var buttonsPressed = [];
 var index = Math.floor(Math.random() * sounds.length);
 buttonsActivated.push(index);
 
-
-document.addEventListener('keydown', function(){
-    if (level == 1 || gameOver == true){
-    var audio = new Audio(sounds[index])
-    divs[index].classList.add("pressed");
-    setTimeout(function(){
-        divs[index].classList.remove("pressed");
-    }, 200);
-    audio.play();
-    title.textContent = "Level 1" 
-    gameOver = false;
-    }
+//initial keydown event
+document.addEventListener('keydown', function() {
+   if (level == 1 || gameOver == true) {
+      var audio = new Audio(sounds[index])
+      divs[index].classList.add("pressed");
+      setTimeout(function() {
+         divs[index].classList.remove("pressed");
+      }, 200);
+      audio.play();
+      title.textContent = "Level 1"
+      gameOver = false;
+   }
 })
 
 
 
-
+// checks if arrays are equal
 function arrayEquals(a, b) {
-    return Array.isArray(a) &&
+   return Array.isArray(a) &&
       Array.isArray(b) &&
       a.length === b.length &&
       a.every((val, index) => val === b[index]);
-  }
-  
+}
+
 
 // adding click event with audio to green button
 greenDiv.addEventListener('click', greenPress)
-function greenPress(){
-    var greenAudio = new Audio("sounds/green.mp3")
-    greenDiv.classList.add("pressed");
-    setTimeout(function(){
-        greenDiv.classList.remove("pressed");
-    }, 200);
-    greenAudio.play();
-    buttonsPressed.push(0)
-    checker();
+
+function greenPress() {
+   var greenAudio = new Audio("sounds/green.mp3")
+   greenDiv.classList.add("pressed");
+   setTimeout(function() {
+      greenDiv.classList.remove("pressed");
+   }, 200);
+   greenAudio.play();
+   buttonsPressed.push(0)
+   checker();
 }
 
 // adding click event with audio to red button
 redDiv.addEventListener('click', redPress)
-function redPress(){
-    var redAudio = new Audio("sounds/red.mp3")
-    redDiv.classList.add("pressed");
-    setTimeout(function(){
-        redDiv.classList.remove("pressed");
-    }, 200);
-    redAudio.play();
-    buttonsPressed.push(1)
-    checker();
+
+function redPress() {
+   var redAudio = new Audio("sounds/red.mp3")
+   redDiv.classList.add("pressed");
+   setTimeout(function() {
+      redDiv.classList.remove("pressed");
+   }, 200);
+   redAudio.play();
+   buttonsPressed.push(1)
+   checker();
 }
 
 // adding click event with audio to yellow button
 yellowDiv.addEventListener('click', yellowPress)
-function yellowPress(){
-    var yellowAudio = new Audio("sounds/yellow.mp3")
-    yellowDiv.classList.add("pressed");
-    setTimeout(function(){
-        yellowDiv.classList.remove("pressed");
-    }, 200);
-    yellowAudio.play();
-    buttonsPressed.push(2)
-    checker();
+
+function yellowPress() {
+   var yellowAudio = new Audio("sounds/yellow.mp3")
+   yellowDiv.classList.add("pressed");
+   setTimeout(function() {
+      yellowDiv.classList.remove("pressed");
+   }, 200);
+   yellowAudio.play();
+   buttonsPressed.push(2)
+   checker();
 }
 
 // adding click event with audio to blue button
 blueDiv.addEventListener('click', bluePress)
-function bluePress(){
-    var blueAudio = new Audio("sounds/blue.mp3")
-    blueDiv.classList.add("pressed");
-    setTimeout(function(){
-        blueDiv.classList.remove("pressed");
-    }, 200);
-    blueAudio.play();
-    buttonsPressed.push(3)
-    checker();
+
+function bluePress() {
+   var blueAudio = new Audio("sounds/blue.mp3")
+   blueDiv.classList.add("pressed");
+   setTimeout(function() {
+      blueDiv.classList.remove("pressed");
+   }, 200);
+   blueAudio.play();
+   buttonsPressed.push(3)
+   checker();
 }
 
-function randomButton(){      
-    var randomIndex = Math.floor(Math.random() * sounds.length);
-    var audio = new Audio(sounds[randomIndex])
-    divs[randomIndex].classList.add("pressed");
-    setTimeout(function(){
-        divs[randomIndex].classList.remove("pressed");
-    }, 200);
-    audio.play();
-    buttonsActivated.push(randomIndex);
+function randomButton() {
+   var randomIndex = Math.floor(Math.random() * sounds.length);
+   var audio = new Audio(sounds[randomIndex])
+   divs[randomIndex].classList.add("pressed");
+   setTimeout(function() {
+      divs[randomIndex].classList.remove("pressed");
+   }, 200);
+   audio.play();
+   buttonsActivated.push(randomIndex);
 }
 
 
+// checks if buttons pressed match buttons activated
+function checker() {
+   var gameOverSound = new Audio("sounds/wrong.mp3");
+   for (var i = 0; i < buttonsActivated.length; i++) {
+      if (buttonsActivated.length == buttonsPressed.length) {
+         if (arrayEquals(buttonsPressed, buttonsActivated)) {
+            level++;
+            buttonsPressed = [];
+            title.textContent = "Level " + level
+            setTimeout(function() {
+               randomButton()
+            }, 1000);
+         } else {
+            gameOverSound.play();
+            buttonsPressed = [];
+            buttonsActivated = [];
+            level = 1;
+            gameOver = true;
+            title.textContent = "Game Over, press any key to restart";
+            document.body.style.backgroundColor = "red";
+            setTimeout(function() {
+               document.body.style.backgroundColor = "#68074f";
+            }, 500);
+            index = Math.floor(Math.random() * sounds.length);
+            buttonsActivated.push(index);
+         }
+      }
+   }
+}
 
-function checker () {
-    for (var i = 0; i <buttonsActivated.length; i++){
-        if (buttonsActivated.length == buttonsPressed.length){
-            if (arrayEquals(buttonsPressed,buttonsActivated)){
-                level++;
-                buttonsPressed = [];
-                title.textContent = "Level " + level 
-                setTimeout(function(){
-                    randomButton()
-                }, 1000); 
-            }else{
-                buttonsPressed = [];
-                buttonsActivated = [];
-                level = 1;
-                gameOver = true;
-                title.textContent = "Game Over, press any key to restart";
-                document.body.style.backgroundColor = "red";
-                setTimeout(function(){
-                    document.body.style.backgroundColor = "#68074f";
-                }, 500); 
-                index = Math.floor(Math.random() * sounds.length);
-                buttonsActivated.push(index);
-            }
-        }
-}
-}
 
 
